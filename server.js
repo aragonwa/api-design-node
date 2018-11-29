2d0a3ce18958c00bdba4fe8c749e6728ef9225bf
Aragon <aragonwa@gmail.com>
2018-11-29 11:43:43 -0800

    adding basic express server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+const express = require('express');
+const app = express();
+const path = require('path');
 
 var jsonData = {count: 12, message: 'hey'};
+console.log(__dirname);
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname+'/index.html')));
+app.get('/data', (req, res)=> res.json(jsonData));
+app.listen(3000, ()=> console.log('hey'));
+
+
